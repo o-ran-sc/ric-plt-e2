4391e59d9d30118894d0df2d4b78f931456a460f
aa7133@att.com <aa7133@att.com>
2020-04-02 13:02:48 +0300

    version 4.0.7 update startup.sh
    Change-Id: Ibc3661e4268d7088312b9db653d976a747eb7b5e
    Signed-off-by: aa7133@att.com <aa7133@att.com>

--- RIC-E2-TERMINATION/startup.sh
+++ RIC-E2-TERMINATION/startup.sh
@@ -7,6 +7,7 @@ fullServiceName=${serviceName}_SERVICE_HOST
 echo "environments service name is $fullServiceName"
 serviceIp=$(env | grep $fullServiceName | awk '{split($0,ip,"="); print ip[2]}')
 echo "service ip is $serviceIp"
-sed -i "s/external-fqdn=e2t.com/external-fqdn=$serviceIp/g" "/opt/e2/config/config.conf"
+sed -i "s/local-ip=127.0.0.1/local-ip=$serviceIp/g" "/opt/e2/config/config.conf"
+sed -i "s/external-fqdn=e2t.com/lexternal-fqdn=$serviceIp/g" "/opt/e2/config/config.conf"
 cat "/opt/e2/config/config.conf"
 ./e2 -p config -f config.conf
